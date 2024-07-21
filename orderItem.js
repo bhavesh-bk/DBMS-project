771b28db56c3ccfce90366e5d71834341fac72d1
bhavesh-bk <bhaveshbk2003@gmail.com>
2024-07-21 21:18:20 +0530

    Delete orderItem.js

--- orderItem.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    const OrderItem = sequelize.define('OrderItem', {
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-    });
-    return OrderItem;
-};
