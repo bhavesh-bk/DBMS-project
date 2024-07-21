8f18f20becc7c2c717b58ece8d7c94c840643a88
bhavesh-bk <bhaveshbk2003@gmail.com>
2024-07-21 21:17:32 +0530

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('food_ordering_db', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
